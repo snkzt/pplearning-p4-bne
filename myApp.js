1294c1335e5f11a7ab9d24788afa2c01ea76fe38
snkzt <snkzt@mercari.com>
2021-02-23 23:13:24 +0900

    Implement a Root-Level Request Logger Middleware - done

--- myApp.js
+++ myApp.js
@@ -1,6 +1,35 @@
 var express = require('express');
 var app = express();
 
+app.use((req, res, next) => {
+console.log(req.method + " " + req.path + " - " + req.ip)
+next();
+})
+
+// function word() {
+//   if (process.env.MESSAGE_STYLE === "uppercase") {
+//     return "Hello json".toUpperCase();
+//   }
+//   return "Hello json";
+// }
+
+// app.get('/json', (req, res) => {
+//   const response = word();
+//   return res.json({'message': response});
+// });
+
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.path} - ${req.ip}`);
+//   next();
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/index.html')
+// });
+
+// app.use('/public', express.static(__dirname + '/public'));
+
+module.exports = app;
 
 
 
